653f6789d08199f27d76c2b3e94473524257671f
Christophe <christophe.lebesnerais@gmail.com>
2015-06-22 21:43:07 +0200

    circles

--- scripts/app.js
+++ scripts/app.js
@@ -1,70 +1,30 @@
-startD3();
-
 function startD3() {
 
     var width = window.innerWidth,
         height = window.innerHeight;
+
+    var svg = d3.select("#d3")
+        .append("svg")
+            .attr("width", width)
+            .attr("height", height);
     
-    var color = d3.scale.category20();
+    var data = d3.range(200)
+        .map(function() { return {x : Math.random() * width, y : Math.random() * height}; });
+    
+    var circles = svg.selectAll("circle").data(data)
+    
+    circles.exit()
+        .transition()
+            .attr("r", 0)
+        .remove();
 
-    var force = d3.layout.force()
-        .charge(-120)
-        .linkDistance(30)
-        .size([width, height]);
+    circles.enter()
+        .append("circle")
+            .attr("r", 0)
+        .transition()
+            .attr("r", 10);
 
-    var svg = d3.select("#d3").append("svg")
-        .attr("width", width)
-        .attr("height", height);
-
-    d3.json("miserables.json", function(error, graph) {
-        force
-            .nodes(graph.nodes)
-            .links(graph.links)
-            .start();
-
-        var link = svg.selectAll(".link")
-            .data(graph.links)
-            .enter().append("line")
-            .attr("class", "link")
-            .style("stroke-width", function(d) {
-                return Math.sqrt(d.value);
-            });
-
-        var node = svg.selectAll(".node")
-            .data(graph.nodes)
-            .enter().append("circle")
-            .attr("class", "node")
-            .attr("r", 5)
-            .style("fill", function(d) {
-                return color(d.group);
-            })
-            .call(force.drag);
-
-        node.append("title")
-            .text(function(d) {
-                return d.name;
-            });
-
-        force.on("tick", function() {
-            link.attr("x1", function(d) {
-                    return d.source.x;
-                })
-                .attr("y1", function(d) {
-                    return d.source.y;
-                })
-                .attr("x2", function(d) {
-                    return d.target.x;
-                })
-                .attr("y2", function(d) {
-                    return d.target.y;
-                });
-
-            node.attr("cx", function(d) {
-                    return d.x;
-                })
-                .attr("cy", function(d) {
-                    return d.y;
-                });
-        });
-    });
+    circles
+        .attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; });
 }

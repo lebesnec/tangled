9d3fa2ee5bcce7f8b963b46d49427ffa44f705e7
Christophe <christophe.lebesnerais@gmail.com>
2016-04-15 00:59:45 +0200

    android error reporting, graphics

--- android/Twisted/app/src/main/java/com/twisted/christophelb/twisted/MainActivity.java
+++ android/Twisted/app/src/main/java/com/twisted/christophelb/twisted/MainActivity.java
@@ -2,6 +2,7 @@ package com.twisted.christophelb.twisted;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
@@ -13,8 +14,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         WebView webView = (WebView) findViewById(R.id.webView);
+        webView.addJavascriptInterface(new JavascriptHandler(), "Android");
+
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+
         webView.loadUrl("file:///android_asset/webapp/index.html");
+    }
+
+    private class JavascriptHandler {
+        @JavascriptInterface
+        public void reportError(String error) throws Exception {
+            throw new Exception(error);
+        }
     }
 }
